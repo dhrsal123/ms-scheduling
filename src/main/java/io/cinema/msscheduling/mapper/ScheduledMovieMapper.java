a0f42a89d1d14e3fc05f30570f30c0bd666123f8
Santiago Gonzalez Diaz <rstdtas@proton.me>
2026-06-30 13:45:00 -0500

    feat: add mapstruct and scheduled movie mapper

---
+++ src/main/java/io/cinema/msscheduling/mapper/ScheduledMovieMapper.java
@@ -0,0 +1,33 @@
+package io.cinema.msscheduling.mapper;
+
+import io.cinema.msscheduling.domain.dto.request.ScheduleMovieRequestDTO;
+import io.cinema.msscheduling.domain.entity.ScheduledMovieEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.UUID;
+
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+
+@Mapper(
+        componentModel = SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS
+)
+public interface ScheduledMovieMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "theaterId", source = "theaterId")
+    @Mapping(target = "branchId", source = "branchId")
+    @Mapping(target = "roomId", source = "dto.roomId")
+    @Mapping(target = "movieId", source = "dto.movieId")
+    @Mapping(target = "start", source = "dto.start")
+    @Mapping(target = "end", source = "dto.end")
+    ScheduledMovieEntity toEntity(
+            ScheduleMovieRequestDTO dto,
+            UUID theaterId,
+            UUID branchId
+    );
+}
