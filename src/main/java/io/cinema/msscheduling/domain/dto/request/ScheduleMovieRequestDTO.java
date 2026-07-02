ff6c46f1e87bcd6bd05e6a853aa2e1cb1266eb5b
Santiago Gonzalez Diaz <rstdtas@proton.me>
2026-07-02 13:37:03 -0500

    feat: add custom annotation for start/end time validation

--- src/main/java/io/cinema/msscheduling/domain/dto/request/ScheduleMovieRequestDTO.java
+++ src/main/java/io/cinema/msscheduling/domain/dto/request/ScheduleMovieRequestDTO.java
@@ -1,5 +1,6 @@
 package io.cinema.msscheduling.domain.dto.request;
 
+import io.cinema.msscheduling.domain.annotations.ValidStartEnd;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
@@ -7,6 +8,7 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@ValidStartEnd
 public record ScheduleMovieRequestDTO(
         @NotNull(message = "movieId can't be null")
         UUID movieId,
