e5099506658baae669f3d586d2b1e00bddee7969
Santiago Gonzalez Diaz <rstdtas@proton.me>
2026-07-02 13:36:23 -0500

    refactor: improve implementation for mapper and repository

--- src/main/java/io/cinema/msscheduling/mapper/ScheduledMovieMapper.java
+++ src/main/java/io/cinema/msscheduling/mapper/ScheduledMovieMapper.java
@@ -1,9 +1,12 @@
 package io.cinema.msscheduling.mapper;
 
 import io.cinema.msscheduling.domain.dto.request.ScheduleMovieRequestDTO;
+import io.cinema.msscheduling.domain.dto.response.ScheduledMovieResponseDTO;
 import io.cinema.msscheduling.domain.entity.ScheduledMovieEntity;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValueCheckStrategy;
 import org.mapstruct.ReportingPolicy;
 
@@ -26,6 +29,23 @@ public interface ScheduledMovieMapper {
     @Mapping(target = "start", source = "dto.start")
     @Mapping(target = "end", source = "dto.end")
     ScheduledMovieEntity toEntity(
+            ScheduleMovieRequestDTO dto,
+            UUID theaterId,
+            UUID branchId
+    );
+
+    ScheduledMovieResponseDTO toDto(ScheduledMovieEntity entity);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "theaterId", source = "theaterId")
+    @Mapping(target = "branchId", source = "branchId")
+    @Mapping(target = "roomId", source = "dto.roomId")
+    @Mapping(target = "movieId", source = "dto.movieId")
+    @Mapping(target = "start", source = "dto.start")
+    @Mapping(target = "end", source = "dto.end")
+    @BeanMapping(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+    ScheduledMovieEntity partialUpdate(
+            @MappingTarget ScheduledMovieEntity scheduledMovieEntity,
             ScheduleMovieRequestDTO dto,
             UUID theaterId,
             UUID branchId
--- src/main/java/io/cinema/msscheduling/repository/ScheduledMoviesRepository.java
+++ src/main/java/io/cinema/msscheduling/repository/ScheduledMoviesRepository.java
@@ -3,6 +3,7 @@ package io.cinema.msscheduling.repository;
 import io.cinema.msscheduling.domain.dto.response.ScheduledMoviesResponseDTO;
 import io.cinema.msscheduling.domain.entity.ScheduledMovieEntity;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
@@ -11,5 +12,5 @@ import reactor.core.publisher.Mono;
 import java.util.UUID;
 
 public interface ScheduledMoviesRepository extends R2dbcRepository<ScheduledMovieEntity, UUID> {
-    Flux<ScheduledMoviesResponseDTO> findAllBy(PageRequest pageable);
+    Flux<ScheduledMovieEntity> findAllBy(Pageable pageable);
 }
