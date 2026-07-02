13602335972cf8bd63d8e97522ea07dd5e8a1215
Santiago Gonzalez Diaz <rstdtas@proton.me>
2026-07-02 13:37:39 -0500

    chore: remove unnecessary dependencies

--- src/main/java/io/cinema/msscheduling/repository/ScheduledMoviesRepository.java
+++ src/main/java/io/cinema/msscheduling/repository/ScheduledMoviesRepository.java
@@ -1,13 +1,9 @@
 package io.cinema.msscheduling.repository;
 
-import io.cinema.msscheduling.domain.dto.response.ScheduledMoviesResponseDTO;
 import io.cinema.msscheduling.domain.entity.ScheduledMovieEntity;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
